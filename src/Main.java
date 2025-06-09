cabd9f3a6ca1a320d9f4adc913af5576416dd8ac
Chintankumar5799 <chintanbhalodia5@gmail.com>
2025-06-09 12:16:39 +0530

    Add jenkins file

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("This is java program for jenkins");
+		System.out.println("Output for java program");
+		System.out.println(new Date());
+	}
+}
